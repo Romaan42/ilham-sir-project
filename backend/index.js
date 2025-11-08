6ab8b6e7366ae7a20f5175287b3a6ea544e01d45
Romaan42 <Romaan4244@gmail.com>
2025-11-08 20:04:31 +0500

    change the isconnected to let

--- backend/index.js
+++ backend/index.js
@@ -132,7 +132,7 @@ app.delete("/student/:id", async (req, res) => {
 //   });
 // });
 
-const isConnected = false;
+let isConnected = false;
 async function connectToDatabase() {
   try {
     await mongoose.connect(process.env.MONGO_URL, {
