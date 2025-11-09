430566b1b53a1c7a1dff4eff12b06f2fdc9cb25d
Romaan42 <Romaan4244@gmail.com>
2025-11-09 15:16:51 +0500

    update the axios config

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -3,6 +3,9 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://ilham-sir-project.vercel.app",
   withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
